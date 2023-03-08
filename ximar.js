fa3f598dde0558222f2afa7a9e9e2f8335d3fb20
isma <112773555+ximar2@users.noreply.github.com>
2023-03-08 13:24:40 -0300

    initial commit

---
+++ ximar.js
@@ -0,0 +1,16 @@
+document.getElementById('aa').addEventListener('click',function(){
+    document.getElementById("text").innerHTML= "soldaduras de estructuras, conocimiento del sistema TIG y al sistema MIG/MAG.relleno de las tuberías con electrodo básico, cómo soldar tuberías con TIG en la raíz y rellenado con electrodo, cómo soldar tuberías de aceros ya sean férreos o inoxidables por medio del sistema TIG, soldadura en cobre, soldadura de las chapas de aluminio por medio del sistema TIG o soldar en titanio entre otros cursos."
+});
+
+document.getElementById('bb').addEventListener('click',function(){
+    document.getElementById("text2").innerHTML="programar los dispositivos para que realicen determinadas actividad.Asi tambien la fabricacion de componentes mecanicos en inplantes mecanicos,para diversas funciones mecanicas"
+});
+
+document.getElementById('cc').addEventListener('click',function(){
+    document.getElementById("text3").innerHTML="Elaboracion de soporte de espalda para diversas tareas que requieren de trabajo forsoso,como levantamiento de objetos pesados,trabajos con brazos extendidos(pintar paredes y techo)"
+     });
+
+     ScrollReveal().reveal('.cabecera', {delay: 100});
+     ScrollReveal().reveal('.nueva-cartas', {delay: 2000});
+     ScrollReveal().reveal('.social', {delay: 1000});
+     
